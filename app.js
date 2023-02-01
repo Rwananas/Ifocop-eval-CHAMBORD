7f0b874fd04cea83071d84dab23bf28cbeff5420
erwannbruneteau <erwannthomas@gmail.com>
2023-02-01 22:45:51 +0100

    page resa et les liens;add img sdb

---
+++ app.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("#booking-form");
+const message = document.querySelector("#message");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  message.innerHTML = "Envoyé!";
+  message.classList.add("success");
+  message.style.display = "block";
+});
